75fb791e93cf918b4707ac9039cc913271037c2f
Lennox Kraemer <lennoxkraemer@icloud.com>
2024-01-01 05:53:08 +0100

    key_watcher helper class

--- everything/screensave/index.js
+++ everything/screensave/index.js
@@ -2,16 +2,55 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const links = [
-  "google.com",
-  "youtube.com",
-  "pwg-wittlich.de",
-  "192.168.1.10"
-]
+class Key_watcher {
+  /**
+   *
+   * @param {Array} keys keys array and call-back function
+   */
+  constructor(keys) {
+    this.keys = keys;
+    console.log(this.keys);
+    this.pressed = [];
+    document.body.addEventListener("keydown", (e) => {
+      // console.log(e);
+      if (!this.pressed.includes(e.key)) {
+        this.pressed.push(e.key);
+      }
+      // console.log(this.pressed);
+      this.execute_callbacks();
+    });
+    document.body.addEventListener("keyup", (e) => {
+      // console.log(e);
+      this.pressed.map((el, inel) => {
+        if (el == e.key) {
+          this.pressed.splice(inel, 1);
+        }
+      });
+      // console.log(this.pressed);
+    });
+  }
 
-async function get_favicon(link){
-  let body = await fetch("https://www.google.com/s2/favicons?domain=" + link).then(x => x.body)
-
-  console.log(body);
+  execute_callbacks() {
+    // console.log(this.keys);
+    for (let index = 0; index < this.keys.length; index++) {
+      let searched_keys = this.keys[index][0]; //array of key comb
+      let callback_func = this.keys[index][1];
+      let is_included = 0;
+      for (let i = 0; i < this.pressed.length; i++) {
+        let pressed_key = this.pressed[i];
+        // console.log(pressed_key);
+        if (searched_keys.includes(pressed_key)) {
+          console.log(searched_keys, searched_keys[index], pressed_key);
+          is_included++;
+          console.log(is_included);
+        }
+      }
+      // console.log(searched_keys);
+      if (searched_keys.length == is_included) {
+        callback_func();
+      }
+    }
+  }
 }
-get_favicon(links[2])
+
+new Key_watcher();
---
+++ everything/screensave/links.js
@@ -0,0 +1,41 @@
+
+const links = ["google.com", "youtube.com", "pwg-wittlich.de", "192.168.1.10", "tagesschau.de"];
+let user_links = []
+try {
+  user_links = JSON.parse(window.localStorage.getItem("user_links"))
+} catch (error) {
+  user_links = []
+}
+
+console.log(user_links)
+
+links.concat(user_links)
+
+
+
+function get_favicon_img(link) {
+  const img = document.createElement("img");
+
+  let url = "https://icon.horse/icon/" + link;
+  img.src = url;
+  img.width = 32;
+  img.height = 32;
+
+  return img;
+}
+
+const link_div = document.querySelector(".links");
+
+links.forEach((x, inx) => {
+  const div = document.createElement("div");
+  const text = document.createElement("a");
+  text.textContent = x
+  text.href = x
+  text.classList.add("big")
+  div.appendChild(get_favicon_img(links[inx]));
+  div.appendChild(text);
+
+
+  link_div.appendChild(div)
+  console.log(link_div);
+});
